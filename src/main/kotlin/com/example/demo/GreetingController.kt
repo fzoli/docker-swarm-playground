b8add75d767fc5f5c4a4949572819e8c6fb993f0
Zoltan Farkas <progfarkas@gmail.com>
2025-07-10 18:08:40 +0200

    better greeting

--- src/main/kotlin/com/example/demo/GreetingController.kt
+++ src/main/kotlin/com/example/demo/GreetingController.kt
@@ -3,17 +3,20 @@ package com.example.demo
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.InetAddress
 import java.util.concurrent.atomic.AtomicInteger
 
 @RestController
 class GreetingController(@param:Value("\${GREETING:Hello}") private val greeting: String) {
 
+    private val hostname: String = InetAddress.getLocalHost().hostName
     private var counter = AtomicInteger(0)
 
     @GetMapping("/greeting")
     fun greet(): String {
         val count = counter.incrementAndGet()
-        return "[$count] $greeting"
+        val formattedCount = String.format("%04d", count)
+        return "[$formattedCount] $greeting from $hostname"
     }
 
 }
